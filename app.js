c0d8bec6c3b5cadd864355cb619ab5594cb7089e
cihat <cihat02020202@gmail.com>
2020-07-08 09:51:48 +0300

    completed project

--- app.js
+++ app.js
@@ -17,12 +17,10 @@ function Quiz(questions) {
 Quiz.prototype.getQuestion = function () {
   return this.questions[this.questionIndex];
 }
-
 //Quiz isFinish
 Quiz.prototype.isFinish = function () {
   return this.questions.length == this.questionIndex;
 }
-
 //Quiz Guess
 Quiz.prototype.guess = function (answer) {
   let question = this.getQuestion();
@@ -31,7 +29,6 @@ Quiz.prototype.guess = function (answer) {
   }
   this.questionIndex++;
 }
-
 
 var q1 = new Question("Hangisi bir hücrede bulunan organeldir? ", ["RNA", "Lizozom", "DNA"], "Lizozom");
 var q2 = new Question("Tarihçilerin Kutbu olarak bilinen dünyaca ünlü tarihçimiz kimdir? ", ["Mehmet Fuat Köprülü", "İlber Ortaylı", "Halil İnalcık"], "Halil İnalcık");
@@ -45,12 +42,30 @@ var q9 = new Question("Hangisi tarihteki Türk devletlerinden biri değildir?", 
 var q10 = new Question("Galatasaray hangi yıl UEFA kupasını almıştır?", ["2000", "2001", "2002"], "2000");
 
 let questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
-
 //Start Quiz
 var quiz = new Quiz(questions);
-
 loadQuestions();
-
+//again button
+function againGame() {
+  var myDiv = document.getElementById("againGame");
+  //creating button element
+  var button = document.createElement('BUTTON');
+  //creating text to be
+  button.classList.add("btn");
+  button.classList.add("btn-primary");
+  var text = document.createTextNode("Tekrar Oyna");
+  /*   button.style.backgroundColor = "white";
+    button.style.color = "black";
+    button.style.border = "2px solid black";
+    button.style.borderRadius = "10px" */
+  //appending text to button
+  button.appendChild(text);
+  //appending button to div
+  myDiv.appendChild(button);
+  button.onclick = function () {
+    location.reload();
+  }
+}
 function loadQuestions() {
   if (quiz.isFinish()) {
     showScore();
@@ -58,8 +73,9 @@ function loadQuestions() {
   else {
     var question = quiz.getQuestion();
     var choices = question.choices;
+    let body = document.querySelector('body');
+    body.style.backgroundImage = "url(https://g7.pngresmi.com/preview/283/853/198/who-wants-to-be-a-millionaire-2014-game-show-television-show-quiz-who-wants-to-be-a-millionaire.jpg)";
     document.querySelector("#question").textContent = question.text;
-
     for (var i = 0; i < choices.length; i++) {
       var element = document.querySelector('#choice' + i);
       element.innerHTML = choices[i];
@@ -76,29 +92,39 @@ function guess(id, guess) {
   }
 }
 function showScore() {
-  let html = `<h2>Score</h2><h4>${quiz.score}</h4>`;
-  document.querySelector('.card-body').innerHTML = html;
+  if (quiz.score == 0) {
+    let oneri = "Yazık Kafana..."
+    let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
+    document.querySelector('.card-body').innerHTML = html;
+    againGame();
+  }
+  else if (quiz.score >= 1 && quiz.score <= 3) {
+    let oneri = "Cahilsinnn..."
+    let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
+    document.querySelector('.card-body').innerHTML = html;
+    againGame();
+  }
+  else if (quiz.score > 3 && quiz.score <= 6) {
+    let oneri = "İdare Ederrr..."
+    let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
+    document.querySelector('.card-body').innerHTML = html;
+    againGame();
+  }
+  else if (quiz.score >= 7 && quiz.score <= 9) {
+    let oneri = "Güzelll..."
+    let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
+    document.querySelector('.card-body').innerHTML = html;
+    againGame();
+  }
+  else if (quiz.score > 9 && quiz.score == 10) {
+    let oneri = "Tebrikler Hepsini Doğru Yaptınız..."
+    let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
+    document.querySelector('.card-body').innerHTML = html;
+    againGame();
+  }
 }
-
 function showProgress() {
   let totalQuestion = quiz.questions.length;
   let questionNumber = quiz.questionIndex + 1;
-  document.querySelector('#progress').innerHTML = 'Question ' + questionNumber + " of " + totalQuestion;
+  document.querySelector('#progress').innerHTML = 'Soru ' + questionNumber + " of " + totalQuestion;
 }
-
-
-/* console.log(quiz.isFinish());
-
-console.log(quiz.getQuestion());
-quiz.guess('Lizozom')
-console.log(quiz.getQuestion());
-quiz.guess('Halil İnalcık');
-console.log(quiz.getQuestion());
-quiz.guess('Penguen');
-console.log(quiz.getQuestion());
-quiz.guess('II.Murat');
-
-console.log(quiz.score);
-
-console.log(quiz.isFinish());
- */
