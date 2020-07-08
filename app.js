22beab19ee3f09e503127de49b9f3457cf4177ed
cihat <cihat02020202@gmail.com>
2020-07-08 09:59:05 +0300

    changed fond-style

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ Quiz.prototype.guess = function (answer) {
 }
 
 var q1 = new Question("Hangisi bir hücrede bulunan organeldir? ", ["RNA", "Lizozom", "DNA"], "Lizozom");
-var q2 = new Question("Tarihçilerin Kutbu olarak bilinen dünyaca ünlü tarihçimiz kimdir? ", ["Mehmet Fuat Köprülü", "İlber Ortaylı", "Halil İnalcık"], "Halil İnalcık");
+var q2 = new Question("Tarihçilerin Kutbu olarak bilinen dünyaca ünlü tarihçimiz kimdir? ", ["Mehmet Fuat Köprülü", "Ilber Ortaylı", "Halil Inalcık"], "Halil Inalcık");
 var q3 = new Question("Hangi hayvan memeli değildir?", ["Yunus", "Yarasa", "Penguen"], "Penguen");
 var q4 = new Question("Fatih Sultan Mehmet’in babası kimdir?", ["Yıldırım Beyazıt", "II.Murat", "I.Mehmet"], "II.Murat");
 var q5 = new Question("Hangi yabancı futbolcu Fenerbahçe forması giymiştir?", ["Simoviç", "Prekazi", "Schumacher"], "Schumacher");
-var q6 = new Question("Magna Carta hangi ülkenin kralıyla yapılmış bir sözleşmedir?", ["Fransa", "İngiltre", "İspanya"], "İngiltre");
+var q6 = new Question("Magna Carta hangi ülkenin kralıyla yapılmış bir sözleşmedir?", ["Fransa", "Ingiltre", "spanya"], "Ingiltre");
 var q7 = new Question("Hangisi periyodik tabloda bulunan bir element değildir?", ["Azot", "Oksijen", "Su"], "Su");
 var q8 = new Question("Hangisi bir doğal sayıdır?", ["0", "2.5", "-1"], "0");
-var q9 = new Question("Hangisi tarihteki Türk devletlerinden biri değildir?", ["Avar Kağanlığı", "Emevi Devleti", "Hun İmparatorluğu"], "Emevi Devleti");
+var q9 = new Question("Hangisi tarihteki Türk devletlerinden biri değildir?", ["Avar Kağanlığı", "Emevi Devleti", "Hun Imparatorluğu"], "Emevi Devleti");
 var q10 = new Question("Galatasaray hangi yıl UEFA kupasını almıştır?", ["2000", "2001", "2002"], "2000");
 
 let questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
@@ -105,7 +105,7 @@ function showScore() {
     againGame();
   }
   else if (quiz.score > 3 && quiz.score <= 6) {
-    let oneri = "İdare Ederrr..."
+    let oneri = "Idare Ederrr..."
     let html = `<h2>Skorunuz</h2><h4>${quiz.score}</h4><p>${oneri}</p>`;
     document.querySelector('.card-body').innerHTML = html;
     againGame();
